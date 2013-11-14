8a6bda3202e23f7f92cec3d9fa715017d0b0b338
Jake Cahoon <jake.cahoon@stone-ware.com>
2013-11-13 21:20:35 -0700

    Upgrade to Minitest 5.0 plugin
    Following tenderlove's minitest-emoji project structure.
    Keeps the same behavior but adds Minitest 5.0 plugin
    compatibility.
    
    Closes #2
    Closes #5

--- lib/minitest/rg.rb
+++ lib/minitest/rg.rb
@@ -1,45 +1,4 @@
-require "minitest/unit"
+require "minitest"
 
-module MiniTest
-  class RG
-    VERSION = "1.1.1"
-
-    COLORS = {
-      '.' => "\e[32m.\e[0m",
-      'E' => "\e[33mE\e[0m",
-      'F' => "\e[31mF\e[0m",
-      'S' => "\e[36mS\e[0m",
-    }
-
-    attr_reader :io, :colors
-
-    def initialize io, colors = COLORS
-      @io     = io
-      @colors = colors
-    end
-
-    def print o
-      io.print(colors[o] || o)
-    end
-
-    def puts o=nil
-      return io.puts if o.nil?
-      if o =~ /(\d+) failures, (\d+) errors/
-        if $1 != '0' || $2 != '0'
-          io.puts "\e[31m#{o}\e[0m"
-        else
-          io.puts "\e[32m#{o}\e[0m"
-        end
-      else
-        io.puts o
-      end
-    end
-
-    def method_missing msg, *args
-      return super unless io.respond_to? msg
-      io.send(msg, *args)
-    end
-  end
-end
-
-MiniTest::Unit.output = MiniTest::RG.new(MiniTest::Unit.output)
+Minitest.load_plugins
+Minitest::RG.rg!
